5fd372ff5e24f9b6b6858ac01a19dbf30753dd7e
imakshita9 <imakshita9@gmail.com>
2024-05-07 21:43:27 +0530

    contains all constants

---
+++ secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "Galaxy"    // replace MySSID with your WiFi network name
+#define SECRET_PASS ""  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2536863     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "5G56FKL0M72PPR95"   // replace XYZ with your channel write API Key
