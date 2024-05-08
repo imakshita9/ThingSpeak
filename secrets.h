69b14c5ea8fae60066fb1a3de0b0d28182bedb87
Akshita Sahuji <100114587+imakshita9@users.noreply.github.com>
2024-05-09 01:18:39 +0530

    constants which can be change

--- secrets.h
+++ secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID "Galaxy"    // replace MySSID with your WiFi network name
-#define SECRET_PASS ""  // replace MyPassword with your WiFi password
+#define SECRET_SSID "Galaxy"    //wifi ssid
+#define SECRET_PASS ""  //wifi password
 
-#define SECRET_CH_ID 2536863     // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "5G56FKL0M72PPR95"   // replace XYZ with your channel write API Key
+#define SECRET_CH_ID 2536863     //thingspeak channel id
+#define SECRET_WRITE_APIKEY "5G56FKL0M72PPR95"   //write api key of thingspeak
